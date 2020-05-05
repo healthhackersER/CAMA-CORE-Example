1f4a6e15b07ab145f70747fe0d6a10a1282e67d2
Felix Gundlack <GundlackFelixDev@gmail.com>
2020-05-05 19:19:49 +0200

    #8 Update Status Info Component to display database information

--- Database/Components/DBStatusInfo.component.tsx
+++ Database/Components/DBStatusInfo.component.tsx
@@ -15,15 +15,21 @@ export class DBInfo extends Component<DBInfoProps, any> {
         /** Success Element with Green Icon */
         if(this.props.db_state.db.Status){
             return <View style={{flex: 0.7, alignItems:"stretch", justifyContent:"flex-end"}}>
+                <Text h3>Platform</Text>
                 <Text>{`${Platform.OS} - [${Platform.Version}]`}</Text>
-                <Text>{JSON.stringify(this.state)}</Text>
+                <Text h3>DB State</Text>
+                <Text>{JSON.stringify(this.props.db_state.db)}</Text>
+                <Text h4>DB</Text>
+                <Text>{JSON.stringify(this.props.db_state.DB)}</Text>
             </View>
         } 
         /** Error Element with Red Icon and Error Message */
         else {
             return <View style={{flex: 0.7, alignItems:"stretch", justifyContent:"flex-end"}}>
                 <Text>SQLite not supported on your system</Text>
+                <Text h3>Platform</Text>
                 <Text>{`${Platform.OS} - [${Platform.Version}]`}</Text>
+                <Text h3>DB State</Text>
                 <Text>{JSON.stringify(this.props.db_state)}</Text>
             </View>
         }
