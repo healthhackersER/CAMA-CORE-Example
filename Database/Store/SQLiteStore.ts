e91828dec4f96515e30f7f9c2fa712f271cb0006
Felix Gundlack <GundlackFelixDev@gmail.com>
2020-05-05 19:20:21 +0200

    #8 Initial SQLiteStore Environment

--- Database/Store/SQLiteStore.ts
+++ Database/Store/SQLiteStore.ts
@@ -2,31 +2,101 @@ import { createStore} from 'redux'
 import {Platform} from 'react-native';
 import * as SQLite from "expo-sqlite";
 
-let db: any = null;
-try {
-  if(SQLite){
-    db = SQLite.openDatabase('cancer-data.sqlite');
-    db.transaction(tx => {
-      tx.executeSql(
-        "drop table if exists Test;"
-      );
-    });
-    db.transaction(tx => {
-      tx.executeSql(
-        "create table if not exists Test (id integer primary key not null, test text);"
-      );
-    });
-  }
-} catch{}
-
 export class Entry {
   id: number;
-  text: string
+  value: string
+}
+
+class DBManager {
+  db: any;
+  tables = [];
+  constructor(db_name) {
+    if(this.isSQLiteAvailable){
+      this.db = this.openDB(db_name);
+    } else {
+      throw new Error("SQLite is not available on your system!")
+    }
+  }
+
+  isSQLiteAvailable(){
+    return ['ios', 'android'].includes(Platform.OS);
+  }
+  openDB(db_name) {
+    if(SQLite){
+      try {
+        this.db = SQLite.openDatabase(db_name);
+        this.getTables();
+      } catch (e) {
+        console.error(`Unable to open database ${db_name} - ${e.message}`);
+      }
+    }
+  }
+  async getTables() {
+    return this.db.transaction(tx => {
+      tx.executeSql("select * from sys.Tables", [], (tx, results) => {
+        var temp = [];
+        for (let i = 0; i < results.rows.length; ++i) {
+          temp.push(results.rows.item(i));
+        }
+        this.tables = temp;
+        console.log(JSON.stringify(temp));
+      });
+    }, (e) => {
+      console.error(`Get TABLES ERROR: ${e.message}`);
+    }, (success) => {
+      console.log("Get TABLES SUCCESS");
+    });
+  }
+  async createTable(table_name) {
+    try {
+      return this.db.transaction(tx => {
+        tx.executeSql(`create table if not exists ? (id integer primary key not null, value text);`, [table_name])
+        tx.executeSql('select * from sys.Tables', [], (_, results) => {
+          var temp = [];
+          for (let i = 0; i < results.rows.length; ++i) {
+            temp.push(results.rows.item(i));
+          }
+          this.tables = temp;
+          console.log(JSON.stringify(temp));
+        });
+      }, (_, e) => {
+        console.error(`ERROR creating table: ${e.message}`);
+      }, (ResultSet) => {
+        console.log(`Table created SUCCESS ${JSON.stringify(ResultSet)}`);
+      }
+      );
+    } catch (e) {
+      console.error(`Unable to create table ${table_name} - ${e.message}`);
+    }
+  }
+
+  async insertEntry(entry: Entry, table_name: String) {
+      return this.db.transaction( tx => {
+        // Add a new entry with higher ID and Value that was inserted in TextField
+        tx.executeSql(`insert into ${table_name} (id, value) values (?, ?)`, [entry.id, entry.value])
+        tx.executeSql(`select * from ${table_name}`, [], (_, { rows }) =>
+          console.log(JSON.stringify(rows))
+        );
+      }, (_, error) => console.error(error.message),
+      ()=> console.log("Insert Entry SUCCESS")); 
+  }
+
+  async getEntries(table_name) {
+    let entries = [];
+    this.db.transaction( tx => {
+      tx.executeSql(`select * from ${table_name}`, [], 
+      (_, { rows }) => {
+        entries = rows._array;
+      }, (_, error) => console.log(error.message));
+    });
+    return entries;
+  }
 }
 
 export class StateModel {
   Entries: Array<Entry>;
   Status: Boolean;
+  DB: DBManager;
   db: {
     Name: string;
     Table: string;
@@ -38,72 +108,27 @@ export class StateModel {
 const defaultState:StateModel = {
   Entries: new Array<Entry>() ,
   Status: true,
+  DB: new DBManager('CaMa_data.db'),
   db: {
     Table: 'Test',
-    Name: 'CaMa_data.sqlite',
+    Name: 'CaMa_data.db',
     Status: false,
     Error: ''
   }
 };
 
-class DBManager {
-  db: any;
-  constructor(db_name, table_name) {
-    if(this.isSQLiteAvailable){
-      this.db = this.openDB(db_name);
-      this.createTable(table_name)
-    } else {
-      throw new Error("SQLite is not available on your system!")
-    }
-  }
 
-  isSQLiteAvailable(){
-    return false;//['ios', 'android'].includes(Platform.OS);
-  }
-  openDB(db_name) {
-    if(SQLite){
-      db = SQLite.openDatabase(db_name);
-    }
-  }
-
-  async createTable(table_name) {
-    db.transaction(tx => {
-      tx.executeSql(`drop table if exists ${table_name};`);
-    });
-    db.transaction(tx => {
-      tx.executeSql(`create table if not exists ${table_name} (id integer primary key not null, test text);`);
-    });
-  }
-
-  async insertEntry(entry: Entry, table_name: String) {
-    let state = defaultState;
-    this.db.transaction( tx => {
-        // Add a new entry with higher ID and Value that was inserted in TextField
-        tx.executeSql(`insert into ${table_name} (id, test) values (?, ?)`, [entry.id, entry.text], false, (_, error) => console.log(error.message));
-    }); 
-    return this.getEntries(table_name)
-  }
-
-  async getEntries(table_name) {
-    let entries = [];
-    this.db.transaction( tx => {
-      tx.executeSql(`select * from ${table_name}`, [], (_, { rows }) => {
-        entries = rows._array;
-      }, (_, error) => console.log(error.message));
-    });
-    return entries;
-  }
-}
 
 function SQLiteStore(state=defaultState, action) {
-  let dbManager: DBManager = null
   try {
-    dbManager = new DBManager(state.db.Name, state.db.Table);
     state.db.Status = true;
-
+    if(state.DB) {
+      state.DB.openDB(state.db.Name);
+      state.DB.createTable(state.db.Table);
+    }
     switch(action.type) {
       case "INSERT":
-        const entries = dbManager.insertEntry(action.value.Entry, state.db.Table);
+        const entries = state.DB.insertEntry(action.value.Entry, state.db.Table);
         return {...state,
           Entries : entries,
           Status: true,
